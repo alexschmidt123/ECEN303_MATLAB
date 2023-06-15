3fe37e13a7e43fa51d9a115dd1e52a960d2fb3cc
Gaoming Lin <gaominglins@gmail.com>
2023-06-14 23:42:36 -0500

    update

---
+++ c2_9.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+coin_flip = np.round(np.random.rand(1));
+die_toss = np.ceil(6*np.random.rand(1));
+dice_toss = np.ceil(6*np.random.rand(2))
+
+print(coin_flip)
+print(die_toss)
+print(dice_toss)
